dcc26985ba6caf7e9bdeee39f9fe44205390da09
James Daily <jaypeedaylee@gmail.com>
2010-11-12 00:18:48 -0500

    OMG

---
+++ RPG/RPG/build/Debug/GNU-MacOSX/_ext/_DOTDOT/_DOTDOT/Documents/Java/RPG/rpg.o.d
@@ -0,0 +1,7 @@
+build/Debug/GNU-MacOSX/_ext/_DOTDOT/_DOTDOT/Documents/Java/RPG/rpg.o:  \
+ ../../Documents/Java/RPG/rpg.cpp ../../Documents/Java/RPG/player.hpp \
+  ../../Documents/Java/RPG/monster.hpp
+
+../../Documents/Java/RPG/player.hpp:
+
+../../Documents/Java/RPG/monster.hpp:
---
+++ RPG/RPG/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=rpg
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/rpg
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=rpg.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/rpg.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=rpg
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/rpg
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=rpg.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/rpg.tar
